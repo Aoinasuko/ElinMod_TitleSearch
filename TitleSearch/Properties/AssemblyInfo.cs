aaf193748983eedf8e9d034dbd5b65443183099d
aoi_nasuko <aoinasuko.nasunasu@gmail.com>
2025-02-10 13:05:41 +0900

    Fix

--- TitleSearch/Properties/AssemblyInfo.cs
+++ TitleSearch/Properties/AssemblyInfo.cs
@@ -14,11 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
-// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
-// その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
-
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
 //      メジャー バージョン
